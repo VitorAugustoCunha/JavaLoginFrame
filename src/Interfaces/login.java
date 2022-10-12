f12419b44935d147d1afa5b26efcee9363e740ea
VitorAugustoCunha <103552917+VitorAugustoCunha@users.noreply.github.com>
2022-10-12 20:10:57 -0300

    Update login.java

--- src/Interfaces/login.java
+++ src/Interfaces/login.java
@@ -1,16 +1,20 @@
-package Interfaces;
+package Telas;
 
 import java.awt.Container;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import Telas.cadastro;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
-import Interfaces.cadastro;
 
-public class login{
+public class login {
 	
 	JFrame logPrincipal;
 	JButton logButton;
@@ -19,26 +23,30 @@ public class login{
 	Container cont;
 	String nomeConfirm, senhaConfirm;
 	Font tituloFonte = new Font("Times New Roman", Font.PLAIN, 15);
-	cadastro fodase = new cadastro();
-
+	
 	public login() {
-
+		cadastro cad = new cadastro();
+		cad.cadastro.setVisible(true);
 		JFrame logPrincipal = new JFrame();
-		logPrincipal.setVisible(true);
 		logPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		JButton cadButton = new JButton("Cadastro");
+		cadButton.setFont(tituloFonte);
 		
 		JButton logButton = new JButton("Login");
 		logButton.setFont(tituloFonte);
 		
 		
-		JLabel nome = new JLabel("Nome: ");
+		JTextArea nome = new JTextArea("Nome: ");
 		nome.setFont(tituloFonte);
+		nome.setEditable(false);
 		
-		JLabel senha = new JLabel("Senha: ");
+		JTextArea senha = new JTextArea("Senha: ");
 		senha.setFont(tituloFonte);
+		senha.setEditable(false);
 		
  		JTextField senhaTF = new JTextField(15);
+ 		
  		senhaTF.setFont(tituloFonte);
  		
  		JTextField nomeTF = new JTextField(15);
@@ -47,35 +55,47 @@ public class login{
  		cont = logPrincipal.getContentPane();
  		cont.setLayout(new FlowLayout());
 		
-		logPrincipal.setBounds(400,400,400,400);
-		cont.add(nome);
-		cont.add(nomeTF);
-		cont.add(senha);
-		cont.add(senhaTF);	
-		cont.add(logButton);
-		logPrincipal.add(cont);
-		
+		logPrincipal.setSize(500,500);
+		logPrincipal.add(nome);
+		logPrincipal.add(nomeTF);
+		logPrincipal.add(senha);
+		logPrincipal.add(senhaTF);	
+		logPrincipal.add(logButton);
+		//cont.add(cadButton);
+
+
 		nomeConfirm = nomeTF.getText();
 		
 		senhaConfirm = senhaTF.getText();
+		
+		
+		
+		logButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (nomeTF.getText().equals(cad.nomeTF) && senhaTF.getText().equals(cad.senhaTF)){
+					JOptionPane.showMessageDialog(null, "Loagado com sucesso");
+				} 
+				else {
+						JOptionPane.showMessageDialog(null, "Algo de errado");
+				}
+
+				}
+				
+			});
+		/* cadButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				logPrincipal.setVisible(false);
+				fodase.cadastro.setVisible(true);
+				
+			}
+		});
+		*/
+		
+		
 	}
 	
-	public void loginTeste() {
-		String n1,n2;
 		
-		n1 = this.senhaConfirm;
-		n2 = this.nomeConfirm;
-		
-		if (n1 == fodase.nomeTF) {
-			if (n2 == fodase.senhaTF) {
-				System.out.println("Cadastro efetuado");
-			} else {
-				System.out.println("Algo errado");
-			}
 
-		}
-		
-	}
 }
 
 
