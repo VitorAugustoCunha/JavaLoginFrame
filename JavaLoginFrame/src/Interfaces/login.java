79126105dac83d7efec8d1ca0cd990989124553c
VitorAugustoCunha <103552917+VitorAugustoCunha@users.noreply.github.com>
2022-10-12 17:30:54 -0300

    Update login.java

--- JavaLoginFrame/src/Interfaces/login.java
+++ JavaLoginFrame/src/Interfaces/login.java
@@ -10,7 +10,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
-public class Interfaces{
+public class login(){
 	
 	JFrame logPrincipal;
 	JButton logButton;
